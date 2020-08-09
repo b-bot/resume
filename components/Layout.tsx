b7d7190683fec1e67a84f6b204a69fb1dfd68b23
b-b0t <byronpolley@live.com>
2020-08-09 23:09:08 +0200

    fix: remove react-ga

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,7 @@
-import React from 'react'
-import { initGA, logPageView } from '../utils/analytics'
+import React from "react";
 
 export default class Layout extends React.Component {
-  componentDidMount () {
-    if (!window.GA_INITIALIZED) {
-      initGA()
-      window.GA_INITIALIZED = true
-    }
-    logPageView()
-  }
-  render () {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    )
+  render() {
+    return <div>{this.props.children}</div>;
   }
 }
